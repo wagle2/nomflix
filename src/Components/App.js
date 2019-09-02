0ece288b11d715d84e2f205e7731ed41a404a34b
wagle2 <archmage.maple@gmail.com>
2019-09-02 16:53:09 +0900

    d

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Router from 'Components/Router';
+import Header from 'Components/Header';
+import GlobalStyles from './GlobalStyles';
+
+function App() {
+  return (
+    <>
+    <Router />
+    <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+import {moviesApi} from "api"
+
+export default class extends React.Component {
+  state = {
+    nowPlaying: null,
+    upcoming: null,
+    popular: null,
+    error: null,
+    loading: true
+  };
+
+  // async 기다려
+ async componentDidMount() {
+    try {
+        //기다려!
+        const {data : {results : nowPlaying}} = await moviesApi.nowPlaying();
+        const {data : {results : upcoming}} = await moviesApi.upcoming();
+        const {data : {results : popular}} = await moviesApi.popular();
+        this.setState({
+            nowPlaying, // nowPlaying: nowPlaying
+            upcoming,
+            popular
+        })
+    } catch {
+        this.setState({
+            error: "Can't find movies information."
+        });
+    } finally {
+        this.setState({
+            loading: false
+        });
+    }
+  }
+
+  render() {
+    const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    return (
+      <HomePresenter
+        nowPlaying={nowPlaying}
+        upcoming={upcoming}
+        popular={popular}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
